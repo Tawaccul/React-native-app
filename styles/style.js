451dc927e0bc757cfc78b4bfc5f78f7582421875
Tawaccul <teps2222@gmail.com>
2022-12-27 22:43:25 +0400

    style.js

---
+++ styles/style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export const gStyle = StyleSheet.create({
+  
+     main : {
+         flex: 1,
+         padding: 20,
+         paddingTop: 60
+     },
+     title: { 
+         fontSize: 25,
+         color: '#333',
+         fontFamily: 'Gt-med',
+         textAlign: 'center',
+     }
+})
